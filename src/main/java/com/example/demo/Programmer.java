d09b9f829818250f289717dd2ea89dbe199a9541
programmingpack <hemantpython123@gmail.com>
2020-07-16 22:20:02 +0530

    Adding new project

---
+++ src/main/java/com/example/demo/Programmer.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Programmer {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "My Hello World";
+	}
+
+}
